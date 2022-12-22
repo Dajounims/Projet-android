08aff1a4fe475297c7ab1664e0b518cbd61bfc30
nimatcha <dajounims@gmail.com>
2022-12-22 10:45:13 +0100

    tests

---
+++ app/src/main/java/com/example/projet/NotificationsActivity.java
@@ -0,0 +1,14 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notifications);
+    }
+}
